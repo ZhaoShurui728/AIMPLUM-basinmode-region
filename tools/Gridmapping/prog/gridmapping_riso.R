6c55ef551b9637628b1c81072d9dba8d840d101e
tmkhsgw <tmkhsgw@gmail.com>
2025-05-19 09:29:01 +0900

    Update grid-country map for half degree. Fill missing countries e.g. NOR, FRA etc.

--- tools/Gridmapping/prog/gridmapping_riso.R
+++ tools/Gridmapping/prog/gridmapping_riso.R
@@ -1,3 +1,4 @@
+# This code is for generating country code map for 0.5 degree
 library(dplyr)
 library(tidyr)
 library(readr)
@@ -34,16 +35,22 @@ df$grid005 <- expand.grid(lon005=seq(-180,180,regmap_resol),
   mutate(lon=floor(lon005*2)/2,lat=floor(lat005*2)/2) %>% 
   st_as_sf(coords = c("lon005", "lat005"), crs = 4326) %>%
   st_join(
-    rnaturalearth::ne_countries(scale = "large", returnclass = "sf")[, c("iso_a3")]
+    rnaturalearth::ne_countries(scale = "large", returnclass = "sf")[, c("iso_a3","name")]
   ) %>%
-  filter(!is.na(iso_a3)) %>%
+  filter(!is.na(iso_a3)| name == "Norway"| name == "France"| name == "Somaliland"| name == "Kosovo") %>%
   rename(RISO = iso_a3) %>%
   mutate(
+    RISO = ifelse(name == "Norway", "NOR", RISO),
+    RISO = ifelse(name == "France", "FRA", RISO),
+    RISO = ifelse(name == "Somaliland", "SOM", RISO),
+    RISO = ifelse(name == "Kosovo", "XKX", RISO),
     lon005 = sf::st_coordinates(.)[, 1],
     lat005 = sf::st_coordinates(.)[, 2]
   ) %>%
+  select(-name) %>%
   st_drop_geometry() %>%
   arrange(lon005,lat005)
+
 
 df$landshare <- df$grid005 %>% 
     mutate(landarea_share=(regmap_resol/0.5)**2) %>% 
@@ -54,12 +61,10 @@ df$landshare_AIM <- df$landshare %>%
 #    inner_join(df$risomap,by='Country') %>% 
     group_by(lon,lat,RISO) %>% summarise(landarea_share=sum(landarea_share),.groups='drop')
 
-write_csv(df$landshare_AIM,file=str_c(output_dir,'/landshare_iso.csv'))
+#write_csv(df$landshare_AIM,file=str_c(output_dir,'/landshare_iso.csv'))
 
 df$landshare_AIM %>% 
     pivot_wider(names_from=RISO,values_from=landarea_share,values_fill=0) %>% 
     wgdx.reshape(symName='landshare',3,tName='RISO',str_c(output_dir,'/landshare_iso.gdx'))
-
-
 
 
